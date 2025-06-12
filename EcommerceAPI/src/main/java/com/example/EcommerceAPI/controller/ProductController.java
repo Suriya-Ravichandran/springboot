83adac985fdc71ce958703507a2f60b657c4d4c0
suriyaravichandran <rsuriya119@gmail.com>
2025-06-12 13:10:09 +0530

    update

--- EcommerceAPI/src/main/java/com/example/EcommerceAPI/controller/ProductController.java
+++ EcommerceAPI/src/main/java/com/example/EcommerceAPI/controller/ProductController.java
@@ -3,9 +3,12 @@ package com.example.EcommerceAPI.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +40,23 @@ public class ProductController {
 		return productrepo.findById(id).orElseThrow(()->new ResourceNotFoundException("Product Not Found With this Id:"+id));
 		
 	}
-	
+	@PutMapping("/{id}")
+	public Product Updateproduct(@RequestBody Product product,@PathVariable Long id) {
+		Product productdata=productrepo.findById(id).orElseThrow(()->new ResourceNotFoundException("Product Not Found With this Id:"+id));
+		productdata.setProduct(product.getProduct());
+		productdata.setDescription(product.getDescription());
+		productdata.setPrice(product.getPrice());
+		productdata.setQuantity(product.getQuantity());
+		productdata.setRateing(product.getRateing());
+		return productrepo.save(productdata);
+		
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteuser(@PathVariable Long id){
+		Product productdata=productrepo.findById(id).orElseThrow(()->new ResourceNotFoundException("Product Not Found With this Id:"+id));
+		productrepo.delete(productdata);
+		return ResponseEntity.ok().build();
+		
+	}
 	
 }
